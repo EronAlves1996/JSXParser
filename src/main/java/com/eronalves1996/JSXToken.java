8f4a60484f635f404aa9b188fe654adc76ecafda
Eron Alves <eron.a.silva@capgemini.com>
2023-03-25 09:43:30 -0300

    test: JSXElementName of JSXSelfCLosingElement

--- src/main/java/com/eronalves1996/JSXToken.java
+++ src/main/java/com/eronalves1996/JSXToken.java
@@ -1,7 +1,18 @@
 package com.eronalves1996;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class JSXToken {
 
+    public List<JSXToken> subTokens() {
+        List<JSXToken> subTokenList = new ArrayList<>();
+    }
+
     public static class JSXSelfClosingElement extends JSXToken {}
+
+    public static class JSXElementName extends JSXToken {
+        public String identifier;
+    }
 
 }
--- src/test/java/com/eronalves1996/SelfClosingTagTest.java
+++ src/test/java/com/eronalves1996/SelfClosingTagTest.java
@@ -2,23 +2,39 @@ package com.eronalves1996;
 
 import org.junit.jupiter.api.Test;
 import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SelfClosingTagTest {
 
-    @Test
-    public void testSelfClosingTag()
-    {
-        JSX parsedInputTag = JSX.parse("""
+    private final JSX parsedInputTag = JSX.parse("""
                 <input />
                 """);
 
+    @Test
+    public void testSelfClosingTag()
+    {
         Iterator<JSXToken> tokens = parsedInputTag.tokens();
         JSXToken token = tokens.next();
 
         assertNotNull(token);
         assertEquals(JSXToken.JSXSelfClosingElement.class, token.getClass());
+    }
+
+    @Test
+    public void testParseAJSXElementNameFromSelfClosingTag() {
+        JSXTokenIterator tokens = parsedInputTag.tokens();
+        JSXToken token = tokens.next();
+
+        List<JSXToken> subTokens = token.subTokens();
+        JSXToken jsxElementName = subTokens.get(0);
+
+        assertEquals(1, subTokens.size());
+        assertEquals(JSXToken.JSXElementName.class, jsxElementName.getClass());
+
+        JSXToken.JSXElementName name = (JSXToken.JSXElementName) jsxElementName;
+        assertEquals("input", name.identifier);
     }
 }
