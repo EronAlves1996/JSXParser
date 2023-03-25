2d306db4e233166a16bc5c5702ba5a264bb5e8d4
Eron Alves <eron.a.silva@capgemini.com>
2023-03-25 11:36:23 -0300

    refacotr: JSXToken.JSXSelfClosingTag.init

--- src/main/java/com/eronalves1996/JSXToken.java
+++ src/main/java/com/eronalves1996/JSXToken.java
@@ -51,17 +51,27 @@ public abstract class JSXToken {
         public JSXSelfClosingElement(String s){
             subTokens = new ArrayList<>();
 
-            String sanitizedElementInfo = s.replace("/>", "");
+            String sanitizedElementInfo = sanitizeElementInfo(s);
             String[] unparsedSubTokens = sanitizedElementInfo.split(" ");
+
+            parseElementName(unparsedSubTokens);
+            parseAttributes(unparsedSubTokens);
+        }
+
+        private void parseElementName(String[] unparsedSubTokens) {
             JSXElementName name = new JSXElementName(unparsedSubTokens[0]);
-
-
             subTokens.add(name);
+        }
 
+        private void parseAttributes(String[] unparsedSubTokens) {
             if(unparsedSubTokens.length > 1){
                 JSXAttributes attributes = new JSXAttributes(Arrays.asList(unparsedSubTokens).subList(1, unparsedSubTokens.length));
                 subTokens.add(attributes);
             }
+        }
+
+        private static String sanitizeElementInfo(String s) {
+            return s.replace("/>", "");
         }
 
         @Override
