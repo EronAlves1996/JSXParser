6ed276d8f6fba18f896b12b27c8ebac22981ac68
Eron Alves <eron.a.silva@capgemini.com>
2023-03-25 09:20:03 -0300

    init project

---
+++ src/test/java/com/eronalves1996/AppTest.java
@@ -0,0 +1,16 @@
+package com.eronalves1996;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
